6c347816bbc74f6119fd39463e76fad10a8b8450
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-01-03 15:18:00 +0000

    Add `Scorer` operator trait (#34)

---
+++ packages/brace-ec/src/core/operator/scorer/mod.rs
@@ -0,0 +1,9 @@
+use crate::core::individual::Individual;
+
+pub trait Scorer {
+    type Individual: Individual;
+    type Score: Ord;
+    type Error;
+
+    fn score(&self, individual: &Self::Individual) -> Result<Self::Score, Self::Error>;
+}

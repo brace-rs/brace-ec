01412e959a28b2b7cec800a452cafc881bfd2e8d
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-01-03 12:48:22 +0000

    Implement `Fitness` for `Reverse` (#30)

--- packages/brace-ec/src/core/fitness.rs
+++ packages/brace-ec/src/core/fitness.rs
@@ -1,9 +1,22 @@
+use std::cmp::Reverse;
+
 use super::individual::Individual;
 
 pub trait Fitness: Individual {
     type Value: Ord;
 
     fn fitness(&self) -> Self::Value;
+}
+
+impl<T> Fitness for Reverse<T>
+where
+    T: Fitness,
+{
+    type Value = Reverse<T::Value>;
+
+    fn fitness(&self) -> Self::Value {
+        Reverse(self.0.fitness())
+    }
 }
 
 macro_rules! impl_fitness {
@@ -23,14 +36,18 @@ impl_fitness!(i8, i16, i32, i64, i128, isize);
 
 #[cfg(test)]
 mod tests {
+    use std::cmp::Reverse;
+
     use super::Fitness;
 
     #[test]
     fn test_fitness() {
         let a = 10_u8;
         let b = 100_i32;
+        let c = Reverse(50_u64);
 
         assert_eq!(a.fitness(), 10);
         assert_eq!(b.fitness(), 100);
+        assert_eq!(c.fitness(), Reverse(50));
     }
 }

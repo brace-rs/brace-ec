451239200924e1872a2325844db9b65a48cf9283
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-01-05 13:50:15 +0000

    Add `TryFromIterator` helper trait

--- packages/brace-ec/src/util/iter.rs
+++ packages/brace-ec/src/util/iter.rs
@@ -1,3 +1,7 @@
+use std::convert::Infallible;
+
+use thiserror::Error;
+
 pub trait Iterable {
     type Item;
 
@@ -86,4 +90,56 @@ impl<T> IterableMut for Option<T> {
     fn iter_mut(&mut self) -> Self::IterMut<'_> {
         self.iter_mut()
     }
+}
+
+pub trait TryFromIterator<T>: Sized {
+    type Error;
+
+    fn try_from_iter<I>(iter: I) -> Result<Self, Self::Error>
+    where
+        I: IntoIterator<Item = T>;
+}
+
+impl<T, const N: usize> TryFromIterator<T> for [T; N] {
+    type Error = TryFromIteratorError;
+
+    fn try_from_iter<I>(iter: I) -> Result<Self, Self::Error>
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let mut iter = iter.into_iter();
+
+        match array_util::try_from_fn(|_| iter.next()) {
+            Some(arr) => Ok(arr),
+            None => Err(TryFromIteratorError::NotEnough),
+        }
+    }
+}
+
+impl<T> TryFromIterator<T> for Vec<T> {
+    type Error = Infallible;
+
+    fn try_from_iter<I>(iter: I) -> Result<Self, Self::Error>
+    where
+        I: IntoIterator<Item = T>,
+    {
+        Ok(iter.into_iter().collect())
+    }
+}
+
+impl<T> TryFromIterator<T> for Option<T> {
+    type Error = Infallible;
+
+    fn try_from_iter<I>(iter: I) -> Result<Self, Self::Error>
+    where
+        I: IntoIterator<Item = T>,
+    {
+        Ok(iter.into_iter().next())
+    }
+}
+
+#[derive(Debug, Error, PartialEq, Eq)]
+pub enum TryFromIteratorError {
+    #[error("not enough items in iterator")]
+    NotEnough,
 }

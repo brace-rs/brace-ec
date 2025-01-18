785eb8e3955be8dc7239a3f2c3c3f41ff9fd7b1e
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-01-18 23:23:52 +0000

    Add `ParFill` selector adapter

--- packages/brace-ec/src/core/operator/selector/fill.rs
+++ packages/brace-ec/src/core/operator/selector/fill.rs
@@ -1,3 +1,4 @@
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
 use thiserror::Error;
 
 use crate::core::population::Population;
@@ -54,6 +55,49 @@ where
     }
 }
 
+#[derive(Clone, Debug, Default)]
+pub struct ParFill<S> {
+    selector: S,
+}
+
+impl<S> ParFill<S> {
+    pub fn new(selector: S) -> Self {
+        Self { selector }
+    }
+}
+
+impl<P, S> Selector<P> for ParFill<S>
+where
+    P: Population<Individual: Send>
+        + for<'a> IntoParallelRefMutIterator<'a, Item = &'a mut P::Individual>
+        + Clone
+        + Sync,
+    S: Selector<P, Output = [P::Individual; 1], Error: Send> + Sync,
+{
+    type Output = P;
+    type Error = FillError<S::Error>;
+
+    fn select<Rng>(&self, population: &P, _: &mut Rng) -> Result<Self::Output, Self::Error>
+    where
+        Rng: rand::Rng + ?Sized,
+    {
+        let mut pop = population.clone();
+
+        pop.par_iter_mut()
+            .map_init(rand::thread_rng, |rng, individual| {
+                let [out] = self.selector.select(population, rng)?;
+
+                *individual = out;
+
+                Ok(())
+            })
+            .collect::<Result<(), S::Error>>()
+            .map_err(FillError::Select)?;
+
+        Ok(pop)
+    }
+}
+
 #[derive(Debug, Error)]
 pub enum FillError<S> {
     #[error("unable to fill population from selector")]
@@ -70,7 +114,7 @@ mod tests {
     use crate::core::population::Population;
 
     #[test]
-    fn test_select() {
+    fn test_select_fill() {
         let population = [1, 2, 3, 4, 5];
 
         let a = population.select(Best.fill()).unwrap();
@@ -80,5 +124,20 @@ mod tests {
         assert_eq!(a, [5; 5]);
         assert_eq!(b, [1; 5]);
         assert_eq!(c, [5, 1, 5, 1, 5]);
+    }
+
+    #[test]
+    fn test_select_par_fill() {
+        let population = [1, 2, 3, 4, 5];
+
+        let a = population.select(Best.par_fill()).unwrap();
+        let b = population.select(Worst.par_fill()).unwrap();
+        let c = population
+            .select(Best.and(Worst).take::<1>().par_fill())
+            .unwrap();
+
+        assert_eq!(a, [5; 5]);
+        assert_eq!(b, [1; 5]);
+        assert_eq!(c, [5; 5]);
     }
 }

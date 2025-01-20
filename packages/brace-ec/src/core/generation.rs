669d1f40ba40db7505112d6ff0416dc53e4df9e8
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-01-20 21:23:12 +0000

    Add `Generation::advance` methods (#79)

--- packages/brace-ec/src/core/generation.rs
+++ packages/brace-ec/src/core/generation.rs
@@ -1,3 +1,7 @@
+use std::ops::AddAssign;
+
+use num_traits::One;
+
 use super::population::Population;
 
 pub trait Generation {
@@ -9,10 +13,34 @@ pub trait Generation {
     fn population(&self) -> &Self::Population;
 
     fn population_mut(&mut self) -> &mut Self::Population;
+
+    fn advance(&mut self);
+
+    fn advance_with(&mut self, population: impl Into<Self::Population>) -> Self::Population {
+        self.advance();
+        std::mem::replace(self.population_mut(), population.into())
+    }
+
+    fn advanced(mut self) -> Self
+    where
+        Self: Sized,
+    {
+        self.advance();
+        self
+    }
+
+    fn advanced_with(mut self, population: impl Into<Self::Population>) -> Self
+    where
+        Self: Sized,
+    {
+        self.advance_with(population);
+        self
+    }
 }
 
 impl<T, P> Generation for (T, P)
 where
+    T: AddAssign + One,
     P: Population,
 {
     type Id = T;
@@ -28,6 +56,10 @@ where
 
     fn population_mut(&mut self) -> &mut Self::Population {
         &mut self.1
+    }
+
+    fn advance(&mut self) {
+        self.0 += T::one();
     }
 }
 

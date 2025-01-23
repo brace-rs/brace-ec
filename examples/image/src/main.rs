4dc04fd0dff616ff22d4188d15405bfaa5112a2c
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-01-23 20:58:41 +0000

    Add `ImageGenerator` to image example (#87)

--- examples/image/src/main.rs
+++ examples/image/src/main.rs
@@ -1,23 +1,21 @@
 mod args;
 mod evolver;
+mod generator;
 mod individual;
 mod renderer;
 mod scorer;
 mod selector;
 
-use anyhow::{Context, Error};
-use brace_ec::core::fitness::FitnessMut;
-use brace_ec::core::individual::scored::Scored;
+use anyhow::Error;
 use brace_ec::core::operator::evolver::Evolver;
-use brace_ec::core::operator::scorer::Scorer;
+use brace_ec::core::operator::generator::Generator;
 use brace_ec_tui::evolver::Terminal;
 use clap::Parser;
 use image::imageops::FilterType;
-use image::GrayImage;
-use rand::Rng;
 
 use self::args::Args;
 use self::evolver::ImageEvolver;
+use self::generator::ImageGenerator;
 use self::individual::Image;
 use self::renderer::ImageRenderer;
 use self::scorer::ImageScorer;
@@ -30,26 +28,14 @@ fn main() -> Result<(), Error> {
         .resize(args.width, args.height, FilterType::Nearest)
         .into_luma8();
 
-    let mut population = Vec::with_capacity(args.population);
     let mut rng = rand::thread_rng();
 
     let scorer = ImageScorer::new(Image::new(image.clone()));
+    let generator = ImageGenerator::new(image.width(), image.height())
+        .score(scorer.clone())
+        .populate::<Vec<_>>(args.population);
 
-    for _ in 0..args.population {
-        let pixels = std::iter::from_fn(|| Some(rng.gen_range(0..255)))
-            .take(image.width() as usize * image.height() as usize)
-            .collect::<Vec<_>>();
-
-        let image = GrayImage::from_vec(image.width(), image.height(), pixels)
-            .context("Invalid image dimensions")?;
-
-        let mut individual = Scored::new(Image::new(image), 0);
-
-        let score = scorer.score(&individual, &mut rng)?;
-
-        individual.set_fitness(score);
-        population.push(individual);
-    }
+    let population = generator.generate(&mut rng)?;
 
     let evolver = Terminal::new(
         ImageEvolver::new(scorer, args.rate, args.parallel),

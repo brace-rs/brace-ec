22b8bf797d11a26fec4fb3463e43ca2d3045a8b0
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-02-03 20:52:25 +0000

    Add `Counter` generator (#109)

--- packages/brace-ec/src/core/operator/generator/mod.rs
+++ packages/brace-ec/src/core/operator/generator/mod.rs
@@ -1,3 +1,4 @@
+pub mod counter;
 pub mod populate;
 pub mod random;
 
@@ -71,45 +72,5 @@ impl<T, E> Generator<T> for Box<dyn DynGenerator<T, E>> {
         Rng: rand::Rng + ?Sized,
     {
         (**self).dyn_generate(&mut rng)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::cell::Cell;
-    use std::convert::Infallible;
-
-    use super::Generator;
-
-    struct Count(Cell<u8>);
-
-    impl Generator<u8> for Count {
-        type Error = Infallible;
-
-        fn generate<Rng>(&self, _: &mut Rng) -> Result<u8, Self::Error>
-        where
-            Rng: rand::Rng + ?Sized,
-        {
-            let n = self.0.get() + 1;
-
-            self.0.set(n);
-
-            Ok(n)
-        }
-    }
-
-    #[test]
-    fn test_generate() {
-        let mut rng = rand::rng();
-
-        let count = Count(Cell::new(0));
-
-        let a = count.generate(&mut rng).unwrap();
-        let b = count.generate(&mut rng).unwrap();
-        let c = count.generate(&mut rng).unwrap();
-
-        assert_eq!(a, 1);
-        assert_eq!(b, 2);
-        assert_eq!(c, 3);
     }
 }
